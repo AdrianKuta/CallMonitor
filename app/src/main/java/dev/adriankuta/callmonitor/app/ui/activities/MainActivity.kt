fbfe2580095bdf4fbdec669116ad0eec63734b5c
Adrian Kuta <adrian.kuta93@gmail.com>
2022-05-02 17:15:45 +0200

    Add permissions Snackbar.

--- app/src/main/java/dev/adriankuta/callmonitor/app/ui/activities/MainActivity.kt
+++ app/src/main/java/dev/adriankuta/callmonitor/app/ui/activities/MainActivity.kt
@@ -1,12 +1,18 @@
 package dev.adriankuta.callmonitor.app.ui.activities
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
 import androidx.activity.viewModels
+import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
+import dev.adriankuta.callmonitor.R
 import dev.adriankuta.callmonitor.app.ui.adapters.CallLogAdapter
 import dev.adriankuta.callmonitor.app.ui.base.BaseActivity
 import dev.adriankuta.callmonitor.app.ui.viewmodels.MainViewModel
 import dev.adriankuta.callmonitor.databinding.ActivityMainBinding
+
 
 @AndroidEntryPoint
 class MainActivity : BaseActivity() {
@@ -41,8 +47,26 @@ class MainActivity : BaseActivity() {
         viewModel.startServer()
     }
 
+    override fun onPermissionsDenied() {
+        super.onPermissionsDenied()
+        showPermissionsSnackbar()
+    }
+
     override fun onResume() {
         super.onResume()
         viewModel.refreshCallLogs()
+    }
+
+    private fun showPermissionsSnackbar() {
+        Snackbar.make(binding.root, R.string.snackbar_no_permissions, Snackbar.LENGTH_LONG)
+            .setAction(R.string.settings) { openAppSettings() }
+            .show()
+    }
+
+    private fun openAppSettings() {
+        val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+        val uri: Uri = Uri.fromParts("package", packageName, null)
+        intent.data = uri
+        startActivity(intent)
     }
 }
